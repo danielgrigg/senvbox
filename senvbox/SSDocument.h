a9898e7d76d2a363153ab63a65a505c39c6ac099
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-07-19 11:29:09 +1000

    Initial Commit

---
+++ senvbox/SSDocument.h
@@ -0,0 +1,13 @@
+//
+//  SSDocument.h
+//  senvbox
+//
+//  Created by Daniel Grigg on 19/07/13.
+//  Copyright (c) 2013 Daniel Grigg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SSDocument : NSDocument
+
+@end
---
+++ senvbox/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  senvbox
+//
+//  Created by Daniel Grigg on 19/07/13.
+//  Copyright (c) 2013 Daniel Grigg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
